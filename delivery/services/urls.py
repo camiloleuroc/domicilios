bf0c324d9157f9f67cf1805eb74db910a40166b4
camiloleuroc <camilolc23@gmail.com>
2025-04-27 17:15:09 -0500

    Function for assign locations to the users

--- delivery/services/urls.py
+++ delivery/services/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterUser, Login
+from .views import RegisterUser, Login, LocationAssign
 
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
+    path('locations/', LocationAssign.as_view(), name='locations'),
 ]
--- delivery/services/views.py
+++ delivery/services/views.py
@@ -1,9 +1,10 @@
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
+from .serializers import UserSerializer, LocationSerializer
 
 class RegisterUser(APIView):
     """View for register a new user customer or driver"""
@@ -47,3 +48,32 @@ class Login(APIView):
             })
 
         return Response({"detail": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
+
+class LocationAssign(APIView):
+    """Assigns a location to the authenticated user."""
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        """Get all locations for the authenticated user."""
+        
+        locations = LocationSerializer.get_user_location(request.user)
+
+        if locations.exists():
+            serializer = LocationSerializer(locations, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": "No locations found."}, status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
+        """Assign a new location to the authenticated user."""
+        data = request.data
+        data['user'] = request.user.id
+
+        serializer = LocationSerializer(data=data)
+
+        if serializer.is_valid():
+            location = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
