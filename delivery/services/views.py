51c271c7daae701c05ef756fbfdb73a73f97f052
camiloleuroc <camilolc23@gmail.com>
2025-04-27 14:35:42 -0500

    Function for the creation of users with their respective tests.

--- delivery/services/views.py
+++ delivery/services/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
 
-# Create your views here.
+class RegisterUser(APIView):
+    """View for register a new user customer or driver"""
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                'id': user.id,
+                'username': user.username,
+                'is_driver': user.is_driver
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
